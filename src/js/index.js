63e62d9257f0d4cff2c5ec676d047b2a711e6030
戸塚孝高 <y047aka@totsukayoshitakanoMac-mini.local>
2019-01-08 15:24:16 +0900

    Update view

--- src/js/index.js
+++ src/js/index.js
@@ -1,27 +1,10 @@
-import { h, app } from "hyperapp"
-import { div, h1, button } from "@hyperapp/html"
+import { app } from "hyperapp"
+import { div, h1, h2, hr, ul, li } from "@hyperapp/html"
 import { Link, Route, location } from "@hyperapp/router"
 
-const Home = () => h("h2", {}, "Home")
-const About = () => h("h2", {}, "About")
-const Topic = ({ match }) => h("h3", {}, match.params.topicId)
-const TopicsView = ({ match }) => (
-  h("div", { key: "topics" }, [
-    h("h2", {}, "Topics"),
-    h("ul", {}, [
-      h("li", {}, [
-        h(Link, { to: `${match.url}/components` }, "Components")
-      ]),
-      h("li", {}, [
-        h(Link, { to: `${match.url}/single-state-tree` }, "Single State Tree")
-      ]),
-      h("li", {}, [
-        h(Link, { to: `${match.url}/routing` }, "Routing")
-      ])
-    ]),
-    h(Route, { path: `${match.path}/:topicId`, render: Topic })
-  ])
-)
+import Home from "./view/Home"
+import About from "./view/About"
+import TopicsView from "./view/TopicsView"
 
 const state = {
   location: location.state
@@ -32,22 +15,22 @@ const actions = {
 }
 
 const view = state => (
-  h("div", {}, [
-    h("ul", {}, [
-      h("li", {}, [
-        h(Link, { to: "/" }, "Home")
+  div({}, [
+    ul({}, [
+      li({}, [
+        Link({ to: "/" }, "Home")
       ]),
-      h("li", {}, [
-        h(Link, { to: "/about" }, "About")
+      li({}, [
+        Link({ to: "/about" }, "About")
       ]),
-      h("li", {}, [
-        h(Link, { to: "/topics" }, "Topics")
+      li({}, [
+        Link({ to: "/topics" }, "Topics")
       ])
     ]),
-    h("hr", {}),
-    h(Route, { path: "/", render: Home }),
-    h(Route, { path: "/about", render: About }),
-    h(Route, { path: "/topics", render: TopicsView })
+    hr({}),
+    Route({ path: "/", render: Home }),
+    Route({ path: "/about", render: About }),
+    Route({ path: "/topics", render: TopicsView })
   ])
 )
 
---
+++ src/js/view/Home.js
@@ -0,0 +1,5 @@
+import { h2 } from "@hyperapp/html"
+
+export default () => state => (
+  h2({}, "Home")
+)
