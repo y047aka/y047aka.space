1b63dd7e5b6c0ee66ca962a64a394c821b17c9ea
戸塚孝高 <y047aka@totsukayoshitakanoMac-mini.local>
2019-02-08 20:21:25 +0900

    Add contents

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,9 @@
+// Libraries
+import axios from "axios"
+
+// Hyperapp
 import { app } from 'hyperapp'
-import { div, span, header, footer, main, nav, h1, h2, p, a, ul, li, img } from '@hyperapp/html'
+import { div, span, header, footer, section, main, nav, h1, h2, p, a, ul, li, img } from '@hyperapp/html'
 
 /*
 import Home from './view/Home'
@@ -7,12 +11,24 @@ import About from './view/About'
 import TopicsView from './view/TopicsView'
 */
 
-const state = {}
+const state = {
+  calender: [{}]
+}
 
-const actions = {}
+const actions = {
+  setCalender: (newObj) => state => ({
+    calender: [newObj]
+  })
+}
 
-const view = state => (
-  div([
+const view = (state, actions) => (
+  div({
+    oncreate: async () => {
+      const path = '../data/WEC_2018-19.json'
+      const response = await axios.get(path).catch(e => { console.log(e) })
+      actions.setCalender(response.data)
+    }
+  }, [
     header({ class: 'site-header' }, [
       div({ class: 'icon' }),
       h1([
@@ -25,28 +41,33 @@ const view = state => (
       ])
     ]),
     main([
-      h1('I\'m belong to...'), 
-      ul([
-        li([
-          a({ href: 'http://spacemgz-telstar.com/', target: '_blank' }, '宇宙広報団体 TELSTAR')
+      section([
+        h1('I\'m belong to...'), 
+        ul([
+          li([
+            a({ href: 'http://spacemgz-telstar.com/', target: '_blank' }, '宇宙広報団体 TELSTAR')
+          ]),
+          li([
+            a({ href: 'https://sorabatake.jp/', target: '_blank' }, '宙畑')
+          ])
         ]),
-        li([
-          a({ href: 'https://sorabatake.jp/', target: '_blank' }, '宙畑')
+        h1('Motor Sports Fun :)'), 
+        ul([
+          li('Aston Martin Racing (WEC)'),
+          li('Corvette Racing (IMSA)')
+        ]),
+        h1('Links'), 
+        ul([
+          li([
+            a({ href: 'https://twitter.com/y047aka', target: '_blank' }, 'Twitter')
+          ]),
+          li([
+            a({ href: 'https://github.com/y047aka', target: '_blank' }, 'GitHub')
+          ])
         ])
       ]),
-      h1('Motor Sports Fun :)'), 
-      ul([
-        li('Aston Martin Racing (WEC)'),
-        li('Corvette Racing (IMSA)')
-      ]),
-      h1('Links'), 
-      ul([
-        li([
-          a({ href: 'https://twitter.com/y047aka', target: '_blank' }, 'Twitter')
-        ]),
-        li([
-          a({ href: 'https://github.com/y047aka', target: '_blank' }, 'GitHub')
-        ])
+      section([
+        state.calender[0].series
       ])
     ]),
     footer({ class: 'site-footer' }, [
