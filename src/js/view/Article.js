08dbd316f4627082612fa56fcf6e550ffef7a0de
戸塚孝高 <y047aka@totsukayoshitakanoMac-mini.local>
2019-02-08 21:47:21 +0900

    Delete view modules

--- src/js/view/Article.js
+++
@@ -1,8 +0,0 @@
-import { article, section, h2 } from "@hyperapp/html"
-
-export default ({ match }) => (state, actions) => (
-  article({ oncreate: () => actions.refresh(match.params.topicId) }, [
-    h2({}, state.currentArticle.attributes.title),
-    section({}, state.currentArticle.body)
-  ])
-)
