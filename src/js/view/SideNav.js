f5cf599f9f50fc457d281ecff2bb86dac2693eab
y047aka <y047aka@gmail.com>
2019-02-20 05:33:43 +0900

    Sort links

--- src/js/view/SideNav.js
+++ src/js/view/SideNav.js
@@ -4,13 +4,13 @@ export default () => state =>
 	nav({ class: 'side-nav' }, [
 		ul([
 			li([
-				a({ href: 'https://twitter.com/y047aka', target: '_blank' }, [
-					img({ src: '/images/twitter.svg' })
+				a({ href: 'https://github.com/y047aka', target: '_blank' }, [
+					img({ src: '/images/github.svg' })
 				])
 			]),
 			li([
-				a({ href: 'https://github.com/y047aka', target: '_blank' }, [
-					img({ src: '/images/github.svg' })
+				a({ href: 'https://twitter.com/y047aka', target: '_blank' }, [
+					img({ src: '/images/twitter.svg' })
 				])
 			])
 		])
