d8de2a71d875982a7b4f79b10b77d2d582a2b615
y047aka <y047aka@gmail.com>
2019-02-20 04:59:49 +0900

    Update style

--- src/js/view/SiteHeader.js
+++ src/js/view/SiteHeader.js
@@ -1,4 +1,4 @@
-import { div, span, header, h1, p, img } from '@hyperapp/html'
+import { div, header, h1, p, img } from '@hyperapp/html'
 
 export default () => state =>
   header({ class: 'site-header' }, [
