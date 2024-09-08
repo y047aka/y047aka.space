5a9340213199e01a384ae5f5a578bb6e7c0cbdfe
Yoshitaka Totsuka <y047aka@gmail.com>
2024-09-08 10:53:19 +0900

    feat: set base layout

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,24 +1,27 @@
 import { css } from 'hono/css'
+import { LinkTile } from '../components/LinkTile'
+import { TopSection } from '../components/TopSection'
 import type { Meta } from '../types'
 
 export default function Top() {
   const posts = import.meta.glob<{ frontmatter: Meta }>('./posts/*.md', {
-    eager: true,
+    eager: true
   })
   return (
-    <div>
-      <h2>Posts</h2>
-      <ul class='article-list'>
+    <TopSection title="Blog Posts">
+      <div class={css`display: flex; flex-direction: column; row-gap: 5px;`}>
         {Object.entries(posts).map(([id, module]) => {
           if (module.frontmatter) {
             return (
-              <li>
-                <a href={`${id.replace(/\.md$/, '')}`}>{module.frontmatter.title}</a>
-              </li>
+              <LinkTile
+                title={module.frontmatter.title}
+                subTitle={module.frontmatter.pubDate}
+                url={`${id.replace(/\.md$/, '')}`}
+              />
             )
           }
         })}
-      </ul>
-    </div>
+      </div>
+    </TopSection>
   )
 }
