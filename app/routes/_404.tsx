b37d5d64c1455a6db43e35435d5565d04b855d47
Yoshitaka Totsuka <y047aka@gmail.com>
2024-09-10 11:21:27 +0900

    chore: remove error and 404 route handlers

--- app/routes/_404.tsx
+++
@@ -1,7 +0,0 @@
-import type { NotFoundHandler } from 'hono'
-
-const handler: NotFoundHandler = (c) => {
-  return c.render(<h1>Not Found!</h1>)
-}
-
-export default handler
--- app/routes/_error.tsx
+++
@@ -1,8 +0,0 @@
-import type { ErrorHandler } from 'hono'
-
-const handler: ErrorHandler = (e, c) => {
-  console.log(e.message)
-  return c.render(<h1>Error!</h1>)
-}
-
-export default handler
