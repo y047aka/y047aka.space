f6f17cacb5caaa46adee9295290cb939f9c4ea79
Yoshitaka Totsuka <y047aka@gmail.com>
2024-09-10 11:20:27 +0900

    chore

--- app/global.d.ts
+++ app/global.d.ts
@@ -3,6 +3,7 @@ import type { Meta } from './types'
 
 declare module 'hono' {
   interface ContextRenderer {
+    // biome-ignore lint/style/useShorthandFunctionType: <explanation>
     (
       content: string | Promise<string>,
       meta?: Meta & { frontmatter: Meta }
